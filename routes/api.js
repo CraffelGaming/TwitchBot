e3297035ac96ad8d9de5d1ec35dfac1b6484b1b8
Craffel <craffel.gaming@gmail.com>
2022-02-07 17:22:09 +0100

    documentation

--- routes/api.js
+++ routes/api.js
@@ -18,8 +18,8 @@ let router = express.Router();
  * /channel:
  *   get:
  *     tags:
- *     - channel
- *     summary: Kanäle
+ *     - Channel
+ *     summary: Liste aller registrierten Kanäle
  *     description: Liste aller registrierten Kanäle
  *     consumes:
  *     - application/json
@@ -58,8 +58,8 @@ router.get("/channel", channels);
  * /channel:
  *   put:
  *     tags:
- *     - channel
- *     summary: Kanäle
+ *     - Channel
+ *     summary: Registriert den angemeldeten Channel
  *     description: Registriert den angemeldeten Channel
  *     consumes:
  *     - application/json
@@ -72,20 +72,325 @@ router.get("/channel", channels);
  *         description: not found
  */
 router.put("/channel", channels);
-router.post("/shop/sell", shop);
+/**
+ * @swagger
+ * /channel/select:
+ *   get:
+ *     tags:
+ *     - Channel
+ *     summary: Zeigt den aktuell azsgewählten Kanal an.
+ *     description: Zeigt den aktuell azsgewählten Kanal an.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/channel/select", channels);
+/**
+ * @swagger
+ * /channel/select:
+ *   post:
+ *     tags:
+ *     - Channel
+ *     summary: Wählt den übergebenen Kanal aus.
+ *     description: Wählt den übergebenen Kanal aus.
+ *     consumes:
+ *     - application/json
+ *     parameters:
+ *     - name: "channel"
+ *       in: "query"
+ *       description: "Twitch Kanal"
+ *       required: true
+ *       type: "string"
+ *       default: "#craffelmat"
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.post("/channel/select", channels);
+/**
+ * @swagger
+ * /object:
+ *   get:
+ *     tags:
+ *     - Object
+ *     summary: Listet alle Gegenstände im Abenteuer auf.
+ *     description: Listet alle Gegenstände im Abenteuer auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/object", objects);
+/**
+ * @swagger
+ * /module:
+ *   get:
+ *     tags:
+ *     - Module
+ *     summary: Listet alle Module auf.
+ *     description: Listet alle Module auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/module", modules);
+/**
+ * @swagger
+ * /statistic/gold:
+ *   get:
+ *     tags:
+ *     - Statistic
+ *     summary: Top 10 Abenteurer nach Gold
+ *     description: Top 10 Abenteurer nach Gold
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/statistic/gold", statistic);
+/**
+ * @swagger
+ * /statistic/experience:
+ *   get:
+ *     tags:
+ *     - Statistic
+ *     summary: Top 10 Abenteurer nach Erfahrung
+ *     description: Top 10 Abenteurer nach Erfahrung
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/statistic/experience", statistic);
+/**
+ * @swagger
+ * /level:
+ *   get:
+ *     tags:
+ *     - Level
+ *     summary: Listet alle möglichen Level der Abenteurer auf.
+ *     description: Listet alle möglichen Level der Abenteurer auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/level", level);
+/**
+ * @swagger
+ * /level/{experience}:
+ *   get:
+ *     tags:
+ *     - Level
+ *     summary: Liefert das Level anhand der Erfahrungspunkte.
+ *     description: Liefert das Level anhand der Erfahrungspunkte.
+ *     consumes:
+ *     - application/json
+ *     parameters:
+ *     - name: "experience"
+ *       in: "path"
+ *       description: "Erfahrungspunkte"
+ *       required: true
+ *       type: "string"
+ *       default: 5000
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/level/:experience", level);
+/**
+ * @swagger
+ * /hero:
+ *   get:
+ *     tags:
+ *     - Hero
+ *     summary: Listet alle Abenteurer auf.
+ *     description: Listet alle Abenteurer auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/hero", hero);
+/**
+ * @swagger
+ * /hero/{handle}:
+ *   get:
+ *     tags:
+ *     - Hero
+ *     summary: Gibt einen Abenteurer anhand der eindeutigen ID zürück.
+ *     description: Gibt einen Abenteurer anhand der eindeutigen ID zürück.
+ *     consumes:
+ *     - application/json
+ *     parameters:
+ *     - name: "handle"
+ *       in: "path"
+ *       description: "ID des Helden"
+ *       required: true
+ *       type: "string"
+ *       default: "1"
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/hero/:handle", hero);
+/**
+ * @swagger
+ * /command:
+ *   get:
+ *     tags:
+ *     - Command
+ *     summary: Listet alle Belfehle auf.
+ *     description: Listet alle Belfehle auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/command", command);
+/**
+ * @swagger
+ * /command/dynamic/say:
+ *   get:
+ *     tags:
+ *     - Command
+ *     summary: Listet alle dynamischen Belfehle auf.
+ *     description: Listet alle dynamischen Belfehle auf.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/command/dynamic/say", command);
+/**
+ * @swagger
+ * /command/{name}:
+ *   get:
+ *     tags:
+ *     - Command
+ *     summary: Gibt einen Befehl anhand des eindeutigen Namens zurück.
+ *     description: Gibt einen Befehl anhand des eindeutigen Namens zurück.
+ *     consumes:
+ *     - application/json
+ *     parameters:
+ *     - name: "name"
+ *       in: "path"
+ *       description: "Name des Befehls"
+ *       required: true
+ *       type: "string"
+ *       default: "loot"
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/command/:name", command);
+/**
+ * @swagger
+ * /shop/sell:
+ *   post:
+ *     tags:
+ *     - Shop
+ *     summary: Verkauft den übergebenen Gegenstand des Abenteurers.
+ *     description: Verkauft den übergebenen Gegenstand des Abenteurers.
+ *     consumes:
+ *     - application/json
+  *     parameters:
+ *     - name: "hero"
+ *       in: "query"
+ *       description: "Name des Abenteurers"
+ *       required: true
+ *       type: "string"
+ *       default: "craffelmat"
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
+router.post("/shop/sell", shop);
+/**
+ * @swagger
+ * /twitch:
+ *   get:
+ *     tags:
+ *     - Twitch
+ *     summary: Anzeige der aktuellen Twitch-Anmeldung.
+ *     description: Anzeige der aktuellen Twitch-Anmeldung.
+ *     consumes:
+ *     - application/json
+ *     responses:
+ *       201:
+ *         description: successful operation
+ *       403:
+ *         description: missing twitch login
+ *       404:
+ *         description: not found
+ */
 router.get("/twitch", twitch);
 //documentation endpoint for Swagger
 router.use('/', swaggerUi.serve, swaggerUi.setup(specs.default));
